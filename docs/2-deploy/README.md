c603d68b9beda72c10a7bfa9c2b244ba1af2c2f4
pipo02mix <fernando@giantswarm.io>
2024-11-10 18:28:39 +0100

    Add GitOps

--- docs/2-deploy/README.md
+++ docs/2-deploy/README.md
@@ -1,15 +1,24 @@
+# Add my Node.js application to the platform
 
+## Create a repository from the template or sync with the template repository
 
+1. Use the GitHub UI to create a new repository from the template repository [`template-node`](https://github.com/mediona/template-node). Alternatively, you can sync your repository with the template repository by following the instructions [here](https://docs.github.com/en/github/creating-cloning-and-archiving-repositories/creating-a-repository-from-a-template).
 
-# How to deploy my app
+The template provides you with a Dockerfile, a GitHub Actions workflow, and a `README.md` file. The workflow builds a Docker image and pushes it to the GitHub Container Registry. The Docker image is tagged with the commit SHA and the `latest` tag. Also it packages the application code and create a Helm chart.
 
-This commands generate the repo and the main structure of the layout.
+2. Adapt the template repository to your needs:
 
-```bash
-flux bootstrap github \
-  --owner=$GITHUB_USER \
-  --repository=infra-fleet \
-  --branch=main \
-  --path=./apps/ \
-  --personal
-```
+    - Replace `$NODE_APP_NAME` with the name of your application.
+    - Update the `README.md` file with your project's information.
+    - Push the changes to your repository.
+    - Check the workflow runs successfully.
+
+3. Add your application code to the configuration repository:
+
+    - Clone or pull the [configuration repository](https://github.com/mediona/build-your-own-platform) to your local machine.
+    - Add your application code to the `apps` directory.
+    - Commit and push the changes to the configuration repository.
+    - Wait for approval if it goes to production, otherwise a team member can approve the changes.
+    - Merge and check the application is deployed successfully.
+
+For next updates all you need to do is to update the HelmRelease resource in the configuration repository with the new image tag. A workflow can be configured to do it for every release you do.
--- platform.sh
+++ platform.sh
@@ -11,20 +11,23 @@ KUBECONFIG_DEV_FILE="/tmp/dev.kubeconfig"
 function bootstrap_platform() {
     echo -e "${BLUE}Platform bootstrap...${NC}"
     echo -e "${GREEN}Kind management cluster creation...${NC}"
-    kind create cluster --name platform --config cluster-config.yaml
+    kind create cluster --name platform --config docs/0-base-platform/cluster-config.yaml
 
-    echo "${GREEN}Installing cluster API components...${NC}"
+    echo -e "${GREEN}Installing cluster API components...${NC}"
     export CLUSTER_TOPOLOGY=true
     clusterctl init
 
     sleep 10
-    echo "${GREEN}Installing Docker as the infrastructure provider...${NC}"
+    echo -e "${GREEN}Installing Docker as the infrastructure provider...${NC}"
     clusterctl init --infrastructure docker
 
     sleep 10
-    echo "${GREEN}Installing teleport as the identity provider...${NC}"
+    echo -e "${GREEN}Installing teleport as the identity provider...${NC}"
     sed "s/\$TELEPORT_AUTH_TOKEN/$TELEPORT_AUTH_TOKEN/" docs/0-base-platform/teleport-config.yaml > /tmp/teleport-config.yaml
-    helm install teleport-agent teleport/teleport-kube-agent -f /tmp/teleport-config.yaml --version 16.4.3 \
+    sed "s/\$CLUSTER_NAME/platform/" /tmp/teleport-config.yaml > /tmp/teleport-config.yaml
+    helm repo add teleport https://charts.releases.teleport.dev
+    helm repo update
+    helm upgrade --install teleport-agent teleport/teleport-kube-agent -f /tmp/teleport-config.yaml --version 16.4.3 \
 --create-namespace --namespace teleport
 }
 
@@ -43,7 +46,7 @@ function create_workload_cluster() {
 
     # Get the port and host from HA PROXY
     port_mapping=$(docker ps --filter "name=$HA_PROXY_CONTAINER_NAME" --format "{{.Ports}}")
-    host_port=$(echo "$port_mapping" | awk -F'->' '{print $1}')
+    host_port="https://"$(echo "$port_mapping" | awk -F'->' '{print $1}')
     echo "The HA Proxy for dev cluster is running on port $host_port"
 
     # Get the kubeconfig file for the dev cluster
@@ -54,8 +57,25 @@ function create_workload_cluster() {
     echo "Updating the server value in the kubeconfig file..."
     yq eval -i '.clusters[] |= select(.name == "dev") .cluster.server = "'"$host_port"'"' "$KUBECONFIG_DEV_FILE"
 
+    sleep 5
     echo -e "${GREEN}Installing the CNI plugin...${NC}"
     kubectl apply --kubeconfig=$KUBECONFIG_DEV_FILE -f https://raw.githubusercontent.com/projectcalico/calico/v3.26.1/manifests/calico.yaml
+
+
+    sleep 10
+    echo -e "${GREEN}Installing teleport as the identity provider...${NC}"
+    export TELEPORT_AUTH_TOKEN=$(tctl tokens add --type=kube --format yaml | yq .token)
+    sed "s/\$TELEPORT_AUTH_TOKEN/$TELEPORT_AUTH_TOKEN/" docs/0-base-platform/teleport-config.yaml > /tmp/teleport-config-dev-tmp.yaml
+    sed "s/\$CLUSTER_NAME/dev/" /tmp/teleport-config-dev-tmp.yaml > /tmp/teleport-config-dev.yaml
+    helm upgrade --kubeconfig=$KUBECONFIG_DEV_FILE --install teleport-agent teleport/teleport-kube-agent -f /tmp/teleport-config-dev.yaml --version 16.4.3 \
+--create-namespace --namespace teleport
+}
+
+function install_flux() {
+    echo -e "${GREEN}Installing Flux...${NC}"
+    kubectl create ns flux-system
+    helm upgrade --install -n flux-system flux oci://ghcr.io/fluxcd-community/charts/flux2
+
 }
 
 function install_crossplane() {
@@ -66,14 +86,14 @@ function install_crossplane() {
       --create-namespace \
       --set provider.packages='{xpkg.upbound.io/crossplane-contrib/provider-aws:v0.39.0}'
 
-    echo "${GREEN}Creating AWS provider secret for Crossplane...${NC}"
+    echo -e "${GREEN}Creating AWS provider secret for Crossplane...${NC}"
     kubectl create secret \
       generic aws-secret \
       -n crossplane-system \
       --from-file=creds=/Users/fernando/.aws/credentials-platform
 
 
-    echo "${GREEN}Deploying the Provider configuration for Crossplane...${NC}"
+    echo -e "${GREEN}Deploying the Provider configuration for Crossplane...${NC}"
     kubectl apply -f crossplane/provider-config.yaml
 }
 
@@ -93,8 +113,8 @@ function show_menu() {
     echo "Please select an option:"
     echo "1) Bootstrap Platform"
     echo "2) Create a workload cluster or development environment"
-    echo "3) Install Crossplane"
-    echo "4) Option 4"
+    echo "3) Install Flux"
+    echo "4) Install Crossplane"
     echo "5) Destroy platform"
     echo "6) Exit"
     read -p "Enter your choice [1-6]: " choice
@@ -111,10 +131,10 @@ while true; do
             create_workload_cluster
             ;;
         3)
-            install_crossplane
+            install_flux
             ;;
         4)
-            option_four
+            install_crossplane
             ;;
         5)
             destroy_platform
